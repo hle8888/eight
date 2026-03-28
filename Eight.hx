a325a6815c863028eaa224ce859968112c354015
hle <hgko155@gmail.com>
2026-03-28 11:02:56 +0500

    Update Eight.hx

--- Eight.hx
+++ Eight.hx
@@ -24,6 +24,9 @@ class Object {
     public var color:Int = 0x000000;
     public var texture:Texture;
 
+    public var show:Bool = true;
+    public var isUI:Bool = false;
+
     public function new(sizeX:Int=61, sizeY:Int=61, texturePath:String=null, _color:Int=0x000000) {
         setSize(sizeX, sizeY);
         color = _color;
@@ -74,6 +77,8 @@ class Object {
     }
 
     public function draw() {
+        if(!show) return;
+
         var _cos:Float = FastTrig.fastcos(getAngle());
         var _sin:Float = FastTrig.fastsin(getAngle());
         var limitX:Int = Std.int(Math.min(sizeX, Engine.n));
@@ -94,9 +99,9 @@ class Object {
 
                 if (texture != null) {
                     var tColor:Int = texture.tex[x][y];
-                    if(tColor != 0x000000) Engine.drawDot(pos.x + rotatedX, pos.y + rotatedY, 0, tColor);
+                    if(tColor != 0x000000) Engine.drawDot(pos.x + rotatedX, pos.y + rotatedY, 0, tColor, isUI);
                 } else {
-                    Engine.drawDot(pos.x + rotatedX, pos.y + rotatedY, 0, color);
+                    Engine.drawDot(pos.x + rotatedX, pos.y + rotatedY, 0, color, isUI);
                 }
             }
         }
@@ -110,12 +115,20 @@ class Object {
         selected = isSelected;
         if (isSelected) { //!Std.isOfType(this, Button)
             Eight.currentSelected = this;
-            
             setOutline(isSelected);
         }
     }
 
     public function checkSelect(mx:Float, my:Float):Bool {
+        //return mx > pos[0] - sizeX/2 && mx < pos[0] + sizeX/2 && my > pos[1] - sizeY/2 && my < pos[1] + sizeY/2;
+        var worldPos = Engine.screenToWorld(mx, my);
+        return worldPos[0] > pos[0] - sizeX/2 
+            && worldPos[0] < pos[0] + sizeX/2 
+            && worldPos[1] > pos[1] - sizeY/2 
+            && worldPos[1] < pos[1] + sizeY/2;
+    }
+
+    public function checkSelectUI(mx:Float, my:Float):Bool {
         return mx > pos[0] - sizeX/2 && mx < pos[0] + sizeX/2 && my > pos[1] - sizeY/2 && my < pos[1] + sizeY/2;
     }
 
@@ -147,6 +160,41 @@ class Object {
         else if(standartTexture != null) texture = standartTexture;
     }
 
+    public var ghostTexture:Texture;
+    public function setGhost(ghost:Bool = true, step:Int = 3) {
+        if (ghost && ghostTexture == null && texture != null) {
+            ghostTexture = new Texture(sizeX, sizeY);
+            standartTexture = new Texture(sizeX, sizeY);
+
+            var limitX = Std.int(Math.min(sizeX, Engine.n));
+            var limitY = Std.int(Math.min(sizeY, Engine.n2));
+
+            for (x in 0...limitX) {
+                for (y in 0...limitY) {
+                    var c = texture.tex[x][y];
+
+                    // сохраняем оригинал
+                    standartTexture.tex[x][y] = c;
+
+                    if (c == 0x000000) {
+                        ghostTexture.tex[x][y] = 0x000000;
+                        continue;
+                    }
+
+                    // сетка
+                    if (x % step == 0 || y % step == 0) {
+                        ghostTexture.tex[x][y] = 0x00FF00; // яркий зелёный
+                    } else {
+                        ghostTexture.tex[x][y] = 0x001100; // слабая зелень (полупрозрачный эффект)
+                    }
+                }
+            }
+        }
+
+        if (ghost) texture = ghostTexture;
+        else if (standartTexture != null) texture = standartTexture;
+    }
+
     public function loadTexture(path:String) {
         texture = TextureManager.loadTexture(path, sizeX, sizeY);
     }
@@ -155,8 +203,6 @@ class Object {
 class Line extends Object {
     public var pos1:Vec3;
     public var pos2:Vec3;
-
-    public var show:Bool = true;
 
     public function new(_pos1:Vec3, _pos2:Vec3, texturePath:String=null, _color:Int=0xD6D3D3) {
         super();
@@ -168,7 +214,7 @@ class Line extends Object {
     }
 
     public override function draw() {
-        if (!show) return;
+        if(!show) return;
 
         var steps = 100; 
         for (i in 0...steps) {
@@ -178,7 +224,7 @@ class Line extends Object {
             var y = pos1[1] + (pos2[1] - pos1[1]) * t;
             var z = pos1[2] + (pos2[2] - pos1[2]) * t;
 
-            Engine.drawDot(x, y, z, color);
+            Engine.drawDot(x, y, z, color, isUI);
         }
     }
 }
@@ -196,6 +242,7 @@ class Circle extends Object {
     }
 
     public override function draw() {
+        if(!show) return;
         var steps = 100;
 
         for (i in 0...steps) {
@@ -205,7 +252,7 @@ class Circle extends Object {
             var py = pos[1] + Math.sin(angle) * r;
             var pz = 0;
 
-            Engine.drawDot(px, py, pz, color);
+            Engine.drawDot(px, py, pz, color, isUI);
         }
     }
 
@@ -218,9 +265,6 @@ class Circle extends Object {
 }
 
 class Eight {
-    public static var zoom:Float = 0.8;
-    public static var cameraOffset = new Vec3(0, 0, 0);
-
     public static var objects:Array<Object> = []; //objects to draw
     public static var currentSelected:Object;
 
@@ -249,6 +293,7 @@ class Eight {
 
     public static var lastTime = haxe.Timer.stamp();
     public static var run:Bool = true;
+    public static var fps:Float = 0.0;
     public function runMainLoop() {
         while(Sdl.processEvents(onEvent) && run) {
             var now = haxe.Timer.stamp();
@@ -262,12 +307,13 @@ class Eight {
             //Engine.drawDot(0, 0, 0); // центр экрана
             //Engine.drawCube(0, 0, 0, 0.5);  // куб в центре экрана, размер 0.5
 
-            Engine.computeShaders();
-            window.present();
-
             var frameTime = (haxe.Timer.stamp() - now);
             var sleep = 1/60 - frameTime;
-            //if (sleep > 0) sdl.Sdl.delay(Std.int(sleep * 1000));
+            if (sleep > 0) sdl.Sdl.delay(Std.int(sleep * 1000));
+            fps = 1.0 / frameTime;
+
+            Engine.computeShaders();
+            window.present();
 
             i = timerCallbacks.length; while(--i > -1) {
                 var timer = timerCallbacks[i];
@@ -320,6 +366,9 @@ class Eight {
 }
 
 class Engine {
+    public static var zoom:Float = 0.8;
+    public static var cameraOffset = new Vec3(0, 0, 0);
+
     static var fullscreenQuad = true;
 
     static var data:hl.Bytes; 
@@ -339,9 +388,16 @@ class Engine {
         data = hl.Bytes.fromBytes(haxe.io.Bytes.alloc(count));
     }
 
+    static var shaderCompute:sdl.Program;
+    static var shaderRender:sdl.Program;
+    static var shaderSpace:sdl.Program;
+    static var ssbo:sdl.Buffer;    
     public static function initShaderEngine() {
         ssbo = GL.createBuffer();
         GL.bindBuffer(GL.SHADER_STORAGE_BUFFER, ssbo);
+
+        //shaderSpace = compileShader(GL.createShader(GL.VERTEX_SHADER), vertexSrcQuad, false);
+        //shaderSpace = compileShader(GL.createShader(GL.FRAGMENT_SHADER), fragSrcQuadSpace, true, shaderSpace);
 
         //shaderCompute = compileShader(GL.createShader(GL.COMPUTE_SHADER), shaderSource);
         shaderRender = compileShader(GL.createShader(GL.VERTEX_SHADER), fullscreenQuad ? vertexSrcQuad : vertexSrc, false);
@@ -392,9 +448,70 @@ class Engine {
             vec4 down  = texture(uColorMap, uv + vec2(0.0, -uTexelSize.y));
             vec4 left  = texture(uColorMap, uv + vec2(-uTexelSize.x, 0.0));
             vec4 right = texture(uColorMap, uv + vec2( uTexelSize.x, 0.0));
-            color = max(max(center, up), max(max(down, left), right)); 
+            //color = max(max(center, up), max(max(down, left), right)); 
+
+            vec4 c = max(max(center, up), max(max(down, left), right));
+            if (c.rgb == vec3(0.0))
+                discard;
+            color = c;
         }
     "; 
+    static var fragSrcQuadSpace = "#version 430
+        in vec2 uv;
+        out vec4 color;
+
+        uniform vec4 uTime;
+        uniform vec4 uCamera;
+
+        // простая noise
+        float noise(vec2 p) {
+            return sin(p.x) * cos(p.y);
+        }
+
+        // fbm (фрактальный шум)
+        float fbm(vec2 p) {
+            float v = 0.0;
+            float a = 0.5;
+
+            for (int i = 0; i < 5; i++) {
+                v += noise(p) * a;
+                p *= 2.0;
+                a *= 0.5;
+            }
+
+            return v;
+        }
+
+        void main() {
+            // нормализуем координаты (-1..1)
+            vec2 p = uv * 2.0 - 1.0;
+
+            // учитываем камеру (движение по миру)
+            vec2 cam = uCamera.xy;
+            p += cam * 0.002;
+
+            // масштаб космоса
+            vec2 q = p * 3.0;
+
+            float time = uTime.x;
+            float v = fbm(q + time * 0.1);
+
+            // нормализация
+            v = v * 0.5 + 0.5;
+
+            // цвет космоса
+            vec3 col = vec3(
+                0.05 + v * 0.4,
+                0.02 + v * 0.2,
+                0.1 + v * 0.8
+            );
+
+            // звезды
+            float stars = step(0.97, fract(sin(dot(p * 100.0, vec2(12.9898,78.233))) * 43758.5453));
+            col += stars * 1.5;
+
+            color = vec4(col, 1.0);
+        }";
     //STANDART PIPELINE
     static var vertexSrc = "#version 430
         in vec3 inPos;
@@ -404,7 +521,6 @@ class Engine {
         uniform mat4 uView;
 
         out vec3 vColor;
-
         void main() {
             //gl_Position = uProjection * uView * vec4(inPos, 1.0);
             gl_Position = vec4(inPos, 1.0);
@@ -418,9 +534,6 @@ class Engine {
             color = vec4(vColor, 1.0);
         }";
 
-    static var shaderCompute:sdl.Program;
-    static var shaderRender:sdl.Program;
-    static var ssbo:sdl.Buffer;    
     public static function compileShader(shader, source, link = true, prog = null) {
         GL.shaderSource(shader, source);
         GL.compileShader(shader);
@@ -452,6 +565,9 @@ class Engine {
         GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.CLAMP_TO_EDGE);
         GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.CLAMP_TO_EDGE);
 
+        //GL.enable(GL.BLEND);
+		//GL.blendFunc(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA);
+
         final vertices = Float32Array.fromArray([
             -1,  1, 0.0, 0.0, 1.0,   // pos(x,y,z), uv(u,v)
             -1, -1, 0.0, 0.0, 0.0,
@@ -476,21 +592,34 @@ class Engine {
 
         /* vbo = GL.createBuffer();
         GL.bindBuffer(GL.ARRAY_BUFFER, vbo);
-
-        // пока пустой буфер
         GL.bufferData(GL.ARRAY_BUFFER, 0, null, GL.DYNAMIC_DRAW);
-
         vao = GL.createVertexArray();
         GL.bindVertexArray(vao);
-
         final posAttrib = GL.getAttribLocation(shaderRender, 'inPos');
         final colAttrib = GL.getAttribLocation(shaderRender, 'inColor');
-
         GL.enableVertexAttribArray(posAttrib);
         GL.vertexAttribPointer(posAttrib, 3, GL.FLOAT, false, 24, 0);
-
         GL.enableVertexAttribArray(colAttrib);
         GL.vertexAttribPointer(colAttrib, 3, GL.FLOAT, false, 24, 12); */
+    }
+
+    public static inline function computeShaders0() {
+        GL.clear(GL.COLOR_BUFFER_BIT);
+        GL.useProgram(shaderSpace);
+        var timeLoc = GL.getUniformLocation(shaderRender, "uTime");
+        if (timeLoc != null) {
+            var tb = new hl.Bytes(16);
+            tb.setF32(0, Eight.lastTime);
+            GL.uniform4fv(timeLoc, tb, 0, 1);
+        }
+        var camLoc = GL.getUniformLocation(shaderRender, "uCamera");
+        if (camLoc != null) {
+            var cb = new hl.Bytes(16);
+            cb.setF32(0, cameraOffset.x);
+            cb.setF32(4, cameraOffset.y);
+            GL.uniform4fv(camLoc, cb, 0, 1);
+        } 
+        GL.drawArrays(GL.TRIANGLE_STRIP, 0, 4);
     }
 
     private static inline function shaderUpdateTexelSize() {
@@ -509,9 +638,6 @@ class Engine {
         GL.useProgram(shaderCompute);
         GL.dispatchCompute(n, n, 1);
         GL.memoryBarrier(GL.SHADER_STORAGE_BARRIER_BIT); */
-
-        
-        
      
         GL.activeTexture(GL.TEXTURE0);
         GL.getBufferSubData(GL.SHADER_STORAGE_BUFFER, 0, data, 0, count);
@@ -519,52 +645,56 @@ class Engine {
 
         GL.useProgram(shaderRender);
         shaderUpdateTexelSize();
-        GL.clear(GL.COLOR_BUFFER_BIT);
+        //GL.clear(GL.COLOR_BUFFER_BIT);
         GL.drawArrays(GL.TRIANGLE_STRIP, 0, 4); //GL.drawArrays(GL.GL_POINTS, 0, count); 
 
         /* var b = new hl.Bytes(vertices.length * 4);
         for (i in 0...vertices.length)
             b.setF32(i * 4, vertices[i]);
-
         GL.bindBuffer(GL.ARRAY_BUFFER, vbo);
         GL.bufferData(GL.ARRAY_BUFFER, vertices.length * 4, b, GL.DYNAMIC_DRAW);
-
         GL.useProgram(shaderRender);
         GL.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT);
-
         GL.bindVertexArray(vao);
-
-        // количество вершин = vertices.length / 3
         GL.drawArrays(GL.TRIANGLES, 0, Std.int(vertices.length / 6)); */
     }
 
-    public static inline function drawDot(x:Float = 0.0, y:Float = 0.0, z:Float = 0.0, color:Int = 0xFF79786B) {
-        if (x < 1 || y < 1 || x > n - 1|| y > n2 - 1) return;
+    public static inline function worldToScreen(x:Float, y:Float):Vec3 {
+        return new Vec3(
+            (x - cameraOffset[0]),
+            (y - cameraOffset[1]),
+            0
+        );
+    }
 
+    public static inline function screenToWorld(x:Float, y:Float):Vec3 {
+        return new Vec3(
+            x + cameraOffset[0],
+            y + cameraOffset[1],
+            0
+        );
+    }
+
+    public static inline function drawDot(x:Float = 0.0, y:Float = 0.0, z:Float = 0.0, color:Int = 0xFF79786B, isUI:Bool=false) {
         var xi = cast x;
         var yi = cast y;
+        if (!isUI) {
+            var screenPos = worldToScreen(x, y);
+            xi = Std.int(screenPos[0]);
+            yi = Std.int(screenPos[1]);
+        }
+        
+        if (xi < 1 || yi < 1 || xi > n - 1|| yi > n2 - 1) return;
+
         data.setI32((yi * n + xi) << 2, color); 
     } 
 
     public static inline function drawPixel(x:Float, y:Float) {
         drawDot(x, y);
     }
-
-    private static function debug() {
-        var s = '';
-        for (i in 0...n*n2*l) {
-            s = s + ' ' + data.getF32(i*4);
-        }
-        Sys.println(s + "\n");
-    }
-
-    public static inline function v(x:Float, y:Float, z:Float, r:Float, g:Float, b:Float) {
-        vertices.push(x);
-        vertices.push(y);
-        vertices.push(z);
-        vertices.push(r);
-        vertices.push(g);
-        vertices.push(b);
+    
+    /* public static inline function v(x:Float, y:Float, z:Float, r:Float, g:Float, b:Float) {
+        vertices.push(x); vertices.push(y); vertices.push(z); vertices.push(r); vertices.push(g); vertices.push(b);
     }
 
     public static inline function unpackColor(color:Int) {
@@ -593,5 +723,5 @@ class Engine {
         v(nx-hx, ny+hy, 0, c.r, c.g, c.b);
         v(nx+hx, ny-hy, 0, c.r, c.g, c.b);
         v(nx+hx, ny+hy, 0, c.r, c.g, c.b);
-    }
+    } */
 }
